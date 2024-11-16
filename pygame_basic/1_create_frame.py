acac1ddb75ec338e653e768c57bacdfa66ae25d2
buskingsue <138076776+buskingsue@users.noreply.github.com>
2024-11-16 11:47:26 +0900

    Update 1_create_frame.py
    가상환경 설정 주석문

--- pygame_basic/1_create_frame.py
+++ pygame_basic/1_create_frame.py
@@ -1,3 +1,18 @@
+# 1. 가상환경 생성
+# python -m venv myenv (가상환경 이름)
+
+# 2. 가상환경 활성화
+#source myenv/bin/activate  # macOS/Linux
+#myenv\Scripts\activate     # Windows
+
+#.\activate (tap키)
+
+# 3. 가상환경 내에서 필요한 패키지 설치
+# pip install <패키지이름>
+
+# 4. 작업 완료 후 가상환경 비활성화
+# deactivate
+
 import pygame
 
 pygame.init()
